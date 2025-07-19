c01690c4c6f36d51bd5eeb9a93dbda769ee793e6
root <root@A.localdomain>
2025-07-19 22:42:55 +0200

    feat:Plent-of-features-implemented-waiting-to-be-tested.'

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const Translator = require('../components/translator.js');
+const translatorObject = new Translator();
+const validLocale = ["american-to-british", "british-to-american"];
 
 module.exports = function (app) {
   
@@ -8,6 +10,51 @@ module.exports = function (app) {
 
   app.route('/api/translate')
     .post((req, res) => {
-      
+      const inputText = req.body.text;
+      const locale = req.body.locale;
+
+      if ( validLocale.indexOf(locale) === -1 ){
+        console.log({
+          "error":"Invalid value for locale field"
+        });
+        res.json({
+          "error": "Invalid value for locale field"
+        });
+      } else if ( inputText.length === 0 ) {
+        console.log({
+          "error":'No text to translate'
+        })
+        res.json({
+          "error":"No text to translate"
+        });
+      } else if (inputText.length !== 0 && validLocale.indexOf(locale) !== -1){
+
+        const localeIndex = validLocale.indexOf(locale);
+        let chosenLocale = null;
+
+        switch(localeIndex){
+
+          case 0:
+            // american - to - british
+            chosenLocale = validLocale[localeIndex];
+            console.log(chosenLocale);
+            break;
+          
+          case 1:
+            // british - to - american
+            chosenLocale = validLocale[localeIndex];
+            console.log(chosenLocale);
+            break;
+          
+          default:
+            console.log({
+              "error":"Locale does not exist"
+            });
+        }
+        res.json({
+        "text": inputText,
+        "translation": "N/A"
+      });
+      }
     });
 };
